a9d50e6738697663b8d0669a178095c231fabf7c
Лхагватөгөлдөр <fr0wnnn@gmail.com>
2023-10-13 10:11:51 +0000

    [add] day6

---
+++ day6-rotate-array/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func reverse(nums []int) {
+	for i := 0; i < len(nums)/2; i++ {
+		nums[i], nums[len(nums)-1-i] = nums[len(nums)-1-i], nums[i]
+	}
+}
+
+func rotate(nums []int, k int) {
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	rotate(nums, 3)
+	fmt.Println(nums)
+}
